c7c67f3cecb30fddb0ef712f26ccb2f52e6f4028
yana-hris <yana.hristova84@gmail.com>
2024-03-06 15:47:21 +0200

    Add Custom Error Page 400 and 404

--- CookTheWeek.Web/Controllers/HomeController.cs
+++ CookTheWeek.Web/Controllers/HomeController.cs
@@ -19,9 +19,14 @@ namespace CookTheWeek.Web.Controllers
 
         
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if(statusCode == 400 || statusCode == 404)
+            {
+                return this.View("Error404");
+            }
+            
+            return View();
         }
     }
 }
--- CookTheWeek.Web/Program.cs
+++ CookTheWeek.Web/Program.cs
@@ -48,7 +48,8 @@ namespace CookTheWeek.Web
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Home/Error/500");
+                app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
 
                 app.UseHsts();
             }
