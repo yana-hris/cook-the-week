9d2a187e4cd9466516edf73b404f833dec425257
yana-hris <yana.hristova.work@gmail.com>
2024-06-03 13:21:10 +0300

    Add ViewModel for the page headings

---
+++ CookTheWeek.Web.ViewModels/PartialViewModels/PageHeadingViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CookTheWeek.Web.ViewModels.Partial_View_Models
+{    
+    public class PageHeadingViewModel
+    {
+        public string? Subheading { get; set; }
+        public string? HeadingSecondary { get; set; }
+    }
+}
