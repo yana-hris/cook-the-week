5a8ae2ef27ed52b1b02fc56472b56ac23b038cc4
yana-hris <yana.hristova.work@gmail.com>
2024-06-19 11:55:39 +0300

    Fix KnockoutJS function for sending json data to EditJson Recipe Controller Action

--- CookTheWeek.Common/obj/Debug/net8.0/CookTheWeek.Common.AssemblyInfo.cs
+++ CookTheWeek.Common/obj/Debug/net8.0/CookTheWeek.Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookTheWeek.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17196ff451f913a030d1fa44a1dab1ba0c47b391")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0e54b97425a8d742d254150b3b5a0d51ea04af3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookTheWeek.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookTheWeek.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CookTheWeek.Services/obj/Debug/net8.0/CookTheWeek.Services.Data.AssemblyInfo.cs
+++ CookTheWeek.Services/obj/Debug/net8.0/CookTheWeek.Services.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookTheWeek.Services.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17196ff451f913a030d1fa44a1dab1ba0c47b391")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0e54b97425a8d742d254150b3b5a0d51ea04af3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookTheWeek.Services.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookTheWeek.Services.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
