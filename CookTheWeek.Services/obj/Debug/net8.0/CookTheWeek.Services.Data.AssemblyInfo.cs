7ab2baa6527b6b4caba8c713ed0429d0002823bc
yana-hris <yana.hristova.work@gmail.com>
2024-06-19 14:24:07 +0300

    Add "isChecked" function for RecipeIngredients fractions/decimals in Edit Recipe ko.viewModel

--- CookTheWeek.Services/obj/Debug/net8.0/CookTheWeek.Services.Data.AssemblyInfo.cs
+++ CookTheWeek.Services/obj/Debug/net8.0/CookTheWeek.Services.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookTheWeek.Services.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a8ae2ef27ed52b1b02fc56472b56ac23b038cc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfe85bf67b2526ecdbde7739328def24a7118e15")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookTheWeek.Services.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookTheWeek.Services.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
