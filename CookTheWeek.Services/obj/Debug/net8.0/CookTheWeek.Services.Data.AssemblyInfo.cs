037e97ffd0172c9e36f119a01f26e8b6994d35fb
yana-hris <yana.hristova.work@gmail.com>
2024-06-27 09:31:04 +0300

    Fix pop-up of validation errors upon adding new Cooking Step in Edit View

--- CookTheWeek.Services/obj/Debug/net8.0/CookTheWeek.Services.Data.AssemblyInfo.cs
+++ CookTheWeek.Services/obj/Debug/net8.0/CookTheWeek.Services.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookTheWeek.Services.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a72922c8dc72a411f8f088c75b58cdebddd8feba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1604967ccefcd3e0e0ee223221f0d450b4a64db")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookTheWeek.Services.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookTheWeek.Services.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CookTheWeek.WebApi/obj/Debug/net8.0/CookTheWeek.WebApi.AssemblyInfo.cs
+++ CookTheWeek.WebApi/obj/Debug/net8.0/CookTheWeek.WebApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookTheWeek.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a72922c8dc72a411f8f088c75b58cdebddd8feba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1604967ccefcd3e0e0ee223221f0d450b4a64db")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookTheWeek.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookTheWeek.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
