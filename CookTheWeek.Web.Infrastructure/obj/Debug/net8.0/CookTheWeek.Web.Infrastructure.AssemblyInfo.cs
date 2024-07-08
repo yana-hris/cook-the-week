a3aafb83eac96f3028a87adc5bf476c97017e7bf
yana-hris <yana.hristova.work@gmail.com>
2024-07-08 14:42:00 +0300

    Add animated navbar toggler icon to all 3 navs

--- CookTheWeek.Web.Infrastructure/obj/Debug/net8.0/CookTheWeek.Web.Infrastructure.AssemblyInfo.cs
+++ CookTheWeek.Web.Infrastructure/obj/Debug/net8.0/CookTheWeek.Web.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookTheWeek.Web.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5034494266377ef6aa9b23160f30a91a7d2d1b52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56efd903f50f8033713aaea3f8080a2ecc26f82a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookTheWeek.Web.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookTheWeek.Web.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
