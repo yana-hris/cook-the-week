24f04e3056426c14dc5d0bc482c8df97fe352877
yana-hris <yana.hristova.work@gmail.com>
2024-07-09 16:19:48 +0300

    Update Add Recipe View to use KnockoutJS

--- CookTheWeek.Services.Data.Models/obj/Debug/net8.0/CookTheWeek.Services.Data.Models.AssemblyInfo.cs
+++ CookTheWeek.Services.Data.Models/obj/Debug/net8.0/CookTheWeek.Services.Data.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookTheWeek.Services.Data.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56efd903f50f8033713aaea3f8080a2ecc26f82a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e880d8cf4ad9d1671dc8ba742b5261fc72e1e8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookTheWeek.Services.Data.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookTheWeek.Services.Data.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
